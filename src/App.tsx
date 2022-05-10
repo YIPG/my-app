ef34d49609e35637400c0650aa960a72a6a5e1df
YIPG <yuyakevinito@gmail.com>
2022-05-10 17:42:42 +0900

    feat: add autokana

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const kanaExtractionPattern = /[^ 　ぁあ-んー]/g;
+const isHiragana = (str: string) => str.match(/^[ぁ-んー　]*$/);
 
 function App() {
+  const [kanji, setKanji] = useState("");
+  const [kana, setKana] = useState("");
+  const [curWord, setCurWord] = useState("");
+  const [onComposition, setOnComposition] = useState(false);
+
+  const handleOnchange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newInput = e.target.value;
+
+    if (!newInput) {
+      setKana("");
+      return;
+    }
+  };
+
+  const handleOnCompositionStart = (
+    e: React.CompositionEvent<HTMLInputElement>
+  ) => {
+    setOnComposition(true);
+  };
+
+  const handleOnCompositionUpdate = (
+    e: React.CompositionEvent<HTMLInputElement>
+  ) => {
+    if (!onComposition) return;
+    const input = e.data;
+
+    if (isHiragana(input)) {
+      setCurWord(input);
+    }
+  };
+
+  const handleOnCompositionEnd = (
+    e: React.CompositionEvent<HTMLInputElement>
+  ) => {
+    setOnComposition(false);
+    console.log(curWord);
+    setKana((prevKana) => prevKana + curWord);
+    setCurWord("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +53,19 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
+        <input
+          onChange={handleOnchange}
+          onCompositionStart={handleOnCompositionStart}
+          onCompositionUpdate={handleOnCompositionUpdate}
+          onCompositionEnd={handleOnCompositionEnd}
+        />
+        <input
+          onChange={(e) => {
+            console.log(e);
+          }}
+          value={kana}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
